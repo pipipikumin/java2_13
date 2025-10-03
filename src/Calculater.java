2394065893f1265d44fb3a7d5d92b361e7cc5321
*^~^* <bsi25006@uhe.ac.jp>
2025-10-03 14:36:18 +0900

    電卓機能の足し算、引き算、かけ算、割り算の追加

--- src/Calculater.java
+++ src/Calculater.java
@@ -1,5 +1,17 @@
 
 public class Calculater {
+	
+	
+	
+	void showStart() {
+		System.out.println("**************");
+		System.out.println("電卓アプリ");
+		System.out.println("**************");
+		
+		System.out.println("実行する計算を１つ選んで数字で答えてください。");
+	}
+	
+	
 	void squareNumber(double decimal) {
 		double answer = decimal * decimal;
 		System.out.println(answer);
@@ -16,12 +28,29 @@ public class Calculater {
 		System.out.println(answer);
 		
 	}
+	 
 	
-	void showStart() {
-		System.out.println("**************");
-		System.out.println("電卓アプリ");
-		System.out.println("**************");
-		
-		System.out.println("実行する計算を選んでください");
+	void add(double decimalAdd1,double decimalAdd2) {
+		double answerAdd = decimalAdd1 + decimalAdd2;
+		System.out.println(answerAdd);
 	}
+	
+	
+	void sub(double decimalSub1,double decimalSub2) {
+		double answerSub = decimalSub1 - decimalSub2;
+		System.out.println(answerSub);
+	}
+	
+	void mul(double decimalMul1,double decimalMul2) {
+		double answerMul = decimalMul1 * decimalMul2;
+		System.out.println(answerMul);
+	}
+	
+	void div(double decimalDiv1,double decimalDiv2) {
+		double answerDiv = decimalDiv1 / decimalDiv2;
+		System.out.println(answerDiv);
+	}
+	
+	
+	
 }
--- src/CalculaterApp.java
+++ src/CalculaterApp.java
@@ -8,35 +8,66 @@ public class CalculaterApp {
 		
 		calc.showStart();
 		
-		System.out.println("\n平方根（a）：階乗（b）:終了（c）");
-		String moji = scanner.nextLine();
+		System.out.println("\n1:足し算");
+		System.out.println("\n2:引き算");
+		System.out.println("\n3:かけ算");
+		System.out.println("\n4:わり算");
+		System.out.println("\n5:平方根を求める");
+		System.out.println("\n6:階乗");
+		int input = scanner.nextInt();
 	
 		
 		
-		if(moji.equals("a")) {
+		if(input==1) {
+			System.out.println("ひとつめの数字を入力してください");
+			double argument1 = scanner.nextDouble();
+			System.out.println("ふたつめの数字を入力してください");
+			double argument2 = scanner.nextDouble();
+			calc.add(argument1,argument2);
 			
-		System.out.println("数字をひとつ入力してください");
-		double argument = scanner.nextDouble();
-		calc.squareNumber(argument);
+		}else if(input==2) {
+			System.out.println("ひとつめの数字を入力してください");
+			double argument1 = scanner.nextDouble();
+			System.out.println("ふたつめの数字を入力してください");
+			double argument2 = scanner.nextDouble();
+			calc.sub(argument1,argument2);
+			
+		}else if(input==3) {
+			System.out.println("ひとつめの数字を入力してください");
+			double argument1 = scanner.nextDouble();
+			System.out.println("ふたつめの数字を入力してください");
+			double argument2 = scanner.nextDouble();
+			calc.mul(argument1,argument2);
+			
+		}else if(input==4) {
+			System.out.println("ひとつめの数字を入力してください");
+			double argument1 = scanner.nextDouble();
+			System.out.println("ふたつめの数字を入力してください");
+			double argument2 = scanner.nextDouble();
+			calc.div(argument1,argument2);
+		}
 		
-		}else if(moji.equals("b")){
-		System.out.println("正の整数をひとつ入力してください");
+		else if(input == 5) {
+			
+			System.out.println("数字をひとつ入力してください");
+			double argument = scanner.nextDouble();
+			calc.squareNumber(argument);
 		
-		
-		int number = scanner.nextInt();
-		calc.factorial(number);
+		}else if(input == 6){
+			System.out.println("正の整数をひとつ入力してください");
+			int number = scanner.nextInt();
+			calc.factorial(number);
 		
 		}else {
 			System.out.println("プログラムを終了します");
-			return;
 		}
+		
 		
 		scanner.close();
 		
+	}	
 		
-		
-	}
+}
 	
 	
 
-}
